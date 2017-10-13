f817a7aae3a1af30b2336d5c447ec10d05645a52
Timothy Cevallos <timcev@gmail.com>
2017-10-13 20:46:52 +0000

    automatically backed up by learn

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -11,8 +11,8 @@ class Filters extends React.Component {
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select 
-            name="type" 
+          <select
+            name="type"
             id="type"
             value={this.props.filters.type}
             onChange={this.handleFilterTypeChange}
@@ -27,7 +27,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.props.onFindPetsClick}>Find pets</button>
         </div>
       </div>
     );
